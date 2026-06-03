165be12695afefbc3346a94add6c5adaac3bcfcd
Amir Mohammad Nasiri <amirsana8587@gmail.com>
2026-06-03 15:37:48 +0330

    Fix Database

--- Stardew/src/main/java/org/example/model/User.java
+++ Stardew/src/main/java/org/example/model/User.java
@@ -1,10 +1,18 @@
 package org.example.model;
 
 
+import jakarta.persistence.*;
 import org.example.model.enums.Question;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false)
     private String username;
+    @Column(nullable = false)
     private String passwordHash;
     private String nickname;
     private String email;
@@ -13,5 +21,6 @@ public class User {
     private String answer;
     private int gamePlayed;
     private int highScore;
+    @Transient
     private Game currentGame;
 }
--- Stardew/src/main/java/org/example/view/AppView.java
+++ Stardew/src/main/java/org/example/view/AppView.java
@@ -1,12 +1,24 @@
 package org.example.view;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.example.model.App;
 import org.example.model.enums.MenuTypes;
+import org.example.model.utilities.Connection;
 
 import java.util.Scanner;
 
 public class AppView {
     public final static Scanner scanner = new Scanner(System.in);
+
+    static {
+        Dotenv.configure()
+                .directory(System.getProperty("user.dir") + "/src/main/java/org/example/config")
+                .filename("env." + System.getenv("APP_MODE").toLowerCase())
+                .systemProperties()
+                .load();
+
+        Connection.getDatabase();
+    }
 
     public void run() {
         while (App.getCurrentMenuType() != MenuTypes.ExitMenu) {
